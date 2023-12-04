956f03f93a23c8f702bdd7f6c08450071719c7d6
Kendy-the <152564351+Kendy-the@users.noreply.github.com>
2023-12-03 23:14:49 -0500

    Add files via upload

---
+++ main_1.py
@@ -0,0 +1,25 @@
+from opeMath.scicalculator import add,substract,multiply,divide
+from opeMath.scicalcul import exponent,factorial
+#add
+result = add(2,5,5,7)
+print(result)
+
+#multiply
+result = multiply(3,4,5,6)
+print(result)
+
+#divide
+result = divide(10,5,2)
+print(result)
+
+#substract
+result = substract(3,4,5,6)
+print(result)
+
+#exponent
+result = exponent(2,3)
+print(result)
+
+#factorial
+result = factorial(5)
+print(result)
